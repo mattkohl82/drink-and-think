61bd47edf4590b6c4978817e06ea362fd99ba12f
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-05-06 19:33:56 -0500

    clean up code and add eror handing for drink name search

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -1,135 +1,190 @@
 $(document).ready(function(){
 
-
-$("#searchBtn1").on("click", function() {
-    $("#searchTerm2").val("");
-    $("#response-container").empty();
-    $("input").empty();
-const searchTerm = document.querySelector("#searchTerm1").value;;
-drinkSearch(searchTerm);
-});
-$("#searchBtn2").on("click", function() {
-    $("#searchTerm1").val("");
-    $("#response-container").empty();
-
-
-const searchTerm = document.querySelector("#searchTerm2").value;;
-ingredientSearch(searchTerm);
-});
-
-
-function ingredientSearch(searchTerm) {
-    console.log(searchTerm)
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+ searchTerm)
-        
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data)
-        $("#response-container-2").empty();
-        var drinkData = "";
-        if (data.drinks.length > 1){
-            drinkData = data.drinks[i]
-            //show the user all of the drinks
-            for (var i = 0; i < data.drinks.length; i++) {// display each drink stored in local storage
-                var drinkType = data.drinks[i].strDrink;
-                var drinkPic = data.drinks[i].strDrinkThumb;
-                console.log (data.drinks[i].strDrink)
-                showDrinks(drinkType, drinkPic);
-            }
-        }
-    })
-}
-
-//display recipe name and picture based on ingredient search
-function showDrinks(drinkType, drinkPic) {
-    var contResults = $("<div>").addClass("drink-card");
-    //var div = $('<div class="response-drinks">');
-    var searchResult = $('<p class="drink-type" id="drink-type">').text(drinkType);
-    var img =  $('<img class="imgIng">').attr("src", drinkPic).attr('id', 'drinkPic').click(function() {
-        var searchTerm = drinkType
-        drinkSearch(searchTerm);
-        //$("#response-container").append(div);
-        //$("#response-drinks").append(searchResult, img);
-        
+    $("input").on("click", function() {
+        $("#searchTerm1").val("");
+        $("#searchTerm2").val("");
     });
-    contResults.append(img, searchResult);
-    $("#response-container").append(contResults);
-}
-
-
-//search by drink name
-function drinkSearch(searchTerm) {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+ searchTerm)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
+    $("#searchBtn1").on("click", function() {
+        $("#searchTerm2").val("");
         $("#response-container").empty();
-        $("#response-container-2").empty();
-        //clears last searched recipe
-        $(".search").empty();
-        var drinkData = "";
+        //$("input").empty();
+    const searchTerm = document.querySelector("#searchTerm1").value;;
+    drinkSearch(searchTerm);
+    });
+    $("#searchBtn2").on("click", function() {
+        $("#searchTerm1").val("");
+        $("#response-container").empty();
+
+
+    const searchTerm = document.querySelector("#searchTerm2").value;;
+    ingredientSearch(searchTerm);
+    });
+
+
+    function ingredientSearch(searchTerm) {
+        console.log(searchTerm)
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+ searchTerm)
+            
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
             console.log(data)
-        //     make them selectable, whichever one they choose should return a number for that drinks' position in the data.drinks array
-        //     the number returned should be used to set drinkData, see next line
-        //     drinkData = data.drinks[x]   where x relates to whichever drink they clicked
-        // } else if (data.drinks.length == 1) {
-        //     drinkData = data.drinks[0]
-        // } else {
-        //     have the user retry because their search did not return a result
-        // }
-
-        drinkData = data.drinks[0]//TO DO:define this var in the if/else if/else appropriately
-        var ingredients = [];
-        var measurements = [];
-
-        //create object from JSON where ingredients are listed
-        var drinkName = $("<h1 class='drink-name'>").text(data.drinks[0].strDrink);
-        var ingredientsTitle = $("<h3 class='ingredients-title'>").text("Ingredients")
-        var img =  $("<img>").attr("src", data.drinks[0].strDrinkThumb).attr('id', 'drinkPic')
-        var driectionsTitle = $("<h3 class='directions-title'>").text("Directions")
-        var triviaBtn = $('<button class="btn grey">').text("Trivia Time!")
-        
-        
-        
-        $("#response-container-2").append(drinkName);
-        $("#response-container-2").append(img);
-        $("#response-container-2").append(ingredientsTitle);
-        
-        //iterate over each key/value in dictionary
-        Object.entries(drinkData).forEach(([key, value]) => {
-            if (value === "" || value === null) {
-                //do nothing
-                //this code skips over null values !== logic will not work
-            } else {//if it has a value
-                //if the item is an ingredient
-                if (key.startsWith("strMeasure")) {
-                        // add measurement to array
-                        measurements.push(value)
-                }
-                if (key.startsWith("strIngredient")) {
-                    // add ingredient to array 
-                    ingredients.push(value)
+            $("#response-container-2").empty();
+            var drinkData = "";
+            if (data.drinks.length > 1){
+                drinkData = data.drinks[i]
+                //show the user all of the drinks
+                for (var i = 0; i < data.drinks.length; i++) {// display each drink stored in local storage
+                    var drinkType = data.drinks[i].strDrink;
+                    var drinkPic = data.drinks[i].strDrinkThumb;
+                    console.log (data.drinks[i].strDrink)
+                    showDrinks(drinkType, drinkPic);
                 }
             }
-        });
-        // display list with measurement and  ingredient 
-        for (var i = 0; i < ingredients.length; i++) {
-            var meas = measurements[i];
-            var ing = ingredients[i];
-            let line = $("<li class='ingredient-list'>").text(meas + " " + ing)
-            $("#response-container-2").append(line)
-        }
-        $("#response-container-2").append(driectionsTitle);
-        var instructions = $("<p class='drink-intructions'>").addClass().text(data.drinks[0].strInstructions);
-        $('p').html($('p').text().replace('.', '.<br>'));
-        $("#response-container-2").append(instructions);
-        $(".search").append(triviaBtn).on('click', function(){
-            window.location = "./trivia.html";    
-       });
-    });
-}
+        })
+    }
 
+    //display recipe name and picture based on ingredient search
+    function showDrinks(drinkType, drinkPic) {
+        var contResults = $("<div>").addClass("drink-card");
+        //var div = $('<div class="response-drinks">');
+        var searchResult = $('<p class="drink-type" id="drink-type">').text(drinkType);
+        var img =  $('<img class="imgIng">').attr("src", drinkPic).attr('id', 'drinkPic').click(function() {
+            var searchTerm = drinkType
+            drinkSearch(searchTerm);
+            //$("#response-container").append(div);
+            //$("#response-drinks").append(searchResult, img);
+            
+        });
+        contResults.append(img, searchResult);
+        $("#response-container").append(contResults);
+    }
+
+
+    //search by drink name
+    function drinkSearch(searchTerm) {
+    
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+ searchTerm)
+        .then((response) => {
+                return response.json();
+        })
+        .then((data) => {
+            $("#response-container").empty();
+            $("#response-container-2").empty();
+            //clears last searched recipe
+            $(".search").empty();
+            var drinkData = "";
+                console.log(data)
+            //var resultsArray = [data];
+                console.log(data.drinks)
+            if (data.drinks == null) {
+                //advise user their search did not return results and offer a random drink recipe
+                console.log("array is empty/null")
+                fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    drinkData = data.drinks[0]
+                    var ingredients = [];
+                    var measurements = [];
+                    //create object from JSON where ingredients are listed
+                    var drinkName = $("<h1>").addClass("recipe-text").text(data.drinks[0].strDrink);
+                    var ingredientsTitle = $("<h3>").addClass("recipe-text").text("Ingredients")
+                    var img =  $("<img>").attr("src", data.drinks[0].strDrinkThumb).attr('id', 'drinkPic')
+                    var directionsTitle = $("<h3>").addClass("recipe-text").text("Directions")
+                    var triviaBtn = $("<button>").addClass("btn grey").text("Trivia Time!")
+                    
+                    $("#response-container-2").append("<p>").addClass("drink-instructions recipe-text").text("We're sorry we couldn't what you were looking for. Please try a new search or this drink instead!")     
+                    $("#response-container-2").append(drinkName);
+                    $("#response-container-2").append(img);
+                    $("#response-container-2").append(ingredientsTitle);
+                    
+                    //iterate over each key/value in dictionary
+                    Object.entries(drinkData).forEach(([key, value]) => {
+                        if (value === "" || value === null) {
+                            //do nothing
+                            //this code skips over null values !== logic will not work
+                        } else {//if it has a value
+                            //if the item is an ingredient
+                            if (key.startsWith("strMeasure")) {
+                                    // add measurement to array
+                                    measurements.push(value)
+                            }
+                            if (key.startsWith("strIngredient")) {
+                                // add ingredient to array 
+                                ingredients.push(value)
+                            }
+                        }
+                    });
+                    // display list with measurement and  ingredient 
+                    for (var i = 0; i < ingredients.length; i++) {
+                        var meas = measurements[i];
+                        var ing = ingredients[i];
+                        let line = $("<li>").addClass("drink-instructions recipe-text").text(meas + " " + ing)
+                        $("#response-container-2").append(line)
+                    }
+                    $("#response-container-2").append(directionsTitle);
+                    var instructions = $("<p>").addClass("drink-instructions recipe-text").text(data.drinks[0].strInstructions);
+                    //$(instructions).text().replace(/\.\s/g, '.<br>');
+                    $("#response-container-2").append(instructions);
+                    $(".search").append(triviaBtn).on('click', function(){
+                        window.location = "./trivia.html";    
+                    });
+                });
+                
+            } else {
+                console.log("array is not empty");
+                drinkData = data.drinks[0]
+                var ingredients = [];
+                var measurements = [];
+
+                //create object from JSON where ingredients are listed
+                var drinkName = $("<h1>").addClass("recipe-text").text(data.drinks[0].strDrink);
+                    var ingredientsTitle = $("<h3>").addClass("recipe-text").text("Ingredients")
+                    var img =  $("<img>").attr("src", data.drinks[0].strDrinkThumb).attr('id', 'drinkPic')
+                    var directionsTitle = $("<h3>").addClass("recipe-text").text("Directions")
+                    var triviaBtn = $("<button>").addClass("btn grey").text("Trivia Time!")
+                            
+                $("#response-container-2").append(drinkName);
+                $("#response-container-2").append(img);
+                $("#response-container-2").append(ingredientsTitle);
+                
+                //iterate over each key/value in dictionary
+                Object.entries(drinkData).forEach(([key, value]) => {
+                    if (value === "" || value === null) {
+                        //do nothing
+                        //this code skips over null values !== logic will not work
+                    } else {//if it has a value
+                        //if the item is an ingredient
+                        if (key.startsWith("strMeasure")) {
+                                // add measurement to array
+                                measurements.push(value)
+                        }
+                        if (key.startsWith("strIngredient")) {
+                            // add ingredient to array 
+                            ingredients.push(value)
+                        }
+                    }
+                });
+                // display list with measurement and  ingredient 
+                for (var i = 0; i < ingredients.length; i++) {
+                    var meas = measurements[i];
+                    var ing = ingredients[i];
+                    let line = $("<li>").addClass("drink-instructions recipe-text").text(meas + " " + ing)
+                    $("#response-container-2").append(line)
+                }
+                $("#response-container-2").append(directionsTitle);
+                var instructions = $("<p>").addClass("drink-intructions").text(data.drinks[0].strInstructions);
+                $('p').html($('p').text().replace('.', '.<br>'));
+                $("#response-container-2").append(instructions);
+                $(".search").append(triviaBtn).on('click', function(){
+                    window.location = "./trivia.html";    
+                });
+                
+            }
+            
+        });
+    }
 });
--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,200 +1,3 @@
-//THIS CODE IS FOR THE HARD CODED QUESTIONS ONLY
-var selectedAmount;
-var selectedCategory;
-var selectedDifficulty;
-var mainPage = 'start';
-var timeLeft = 120;
-var totalScore = 0;
-const timerEl = document.getElementById("countdown");
-const timerElzero = document.getElementById("countdown-0");
-const scoreText = document.getElementById("score");
-const question = document.getElementById("question");
-const triviaEl = document.getElementById("game-container");
-const gameEl = document.getElementById("game");
-const choices = Array.from($(".choice-text"));
-const scorePage = document.getElementById("score-container");
-const displayScoreVal = document.getElementById("total-score");
-const submitScore = document.getElementById("submit-btn");
-const initials = document.getElementById("initials");
-const initialsEntered = document.getElementById('initials-entered');
-var highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the score, or the initial value if empty 
-const MAX_HIGH_SCORES = 5;
-let currentQuestion = {};
-let questionCounter = 0;
-let availableQuestions = [];
-let acceptingAnswers = false;//
-const MAX_QUESTIONS = 5;//
-
-//$("#game-container").hide();
-
-// start game when play button is clicked
-$("#playBtn").on("click", function(e) {
-    e.preventDefault();
-    document.getElementById(mainPage).style.display = "none";
-    triviaEl.classList.remove('hide');
-    //$("#game-container").show();
-    playGame();
-});
-//convert html Characters in array back to text before displaying
-function decodeHtml(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-}
-// start game with score reset to 0
-// if timer runs out 
-playGame = () => {
-    questionCounter = 0;
-    totalScore = 0;
-    availableQuestions = [...questions];
-    
-    var timeInterval = setInterval(function () {
-        timeLeft--;
-        timerEl.textContent = timeLeft;
-
-    if (timeLeft === 0) {
-            clearInterval(timeInterval);
-        }
-    }, 1000);
-getQuestion();
-};
-getQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        //save score to local storage
-        localStorage.setItem("mostRecentScore", totalScore);
-        //go to the end to have user enter initials
-        displayScoreVal.textContent = totalScore;
-        endGame();
-    }
-    //display question and answer choices
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    var questionInput = currentQuestion.question;
-    var questionOutput = decodeHtml(questionInput);
-    question.innerText = questionOutput;
-    
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        var answerInput = currentQuestion["choice" + number]
-        var answerOutput = decodeHtml(answerInput);
-        choice.innerText = answerOutput;
-        });
-    
-        availableQuestions.splice(questionIndex, 1);
-        acceptingAnswers = true;
-};
-//add 10 points if correct answer selected, remove 5 points and 15 seconds if wrong answer selected, show selected answer as red/green based on corrent/wrong  
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers) return;
-        acceptingAnswers = false;
-
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
-
-        const answerStatus =
-            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-    
-        if (answerStatus === "correct") {
-            totalScore += 10;
-            //console.log("correct answer selcted, total score "+ totalScore);
-        } else {
-            //console.log("wrong answer selcted, total score "+ totalScore);
-            if(timeLeft >= 15) {
-                timeLeft -= 15;
-            } else {
-                timeLeft = 0;
-            }
-            if(totalScore >= 5) {
-                totalScore -= 5;
-                //console.log("total score "+ totalScore);
-            } else {
-                totalScore = 0;
-            }
-        }
-        scoreText.innerText = totalScore;
-        selectedChoice.parentElement.classList.add(answerStatus);       
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(answerStatus);
-            getQuestion();
-        }, 1000);
-    });
-});
-
-function endGame () {
-    gameEl.classList.add('hide');
-    scorePage.classList.remove('hide');
-
-var mostRecentScore = localStorage.getItem('mostRecentScore');
-
-    submitScore.addEventListener('click', function(e) {
-        e.preventDefault();
-        
-        const score = {
-            score: mostRecentScore,
-            initials: initials.value
-        };
-        if (initials.value === "") {
-            //window.alert("Initials cannot be blank");
-        } else {
-            highScores.push(score);
-
-            highScores.sort((a, b) => b.score - a.score); //sorts highscores in order
-            highScores.splice(5); //keeps top 5 scores in highscore local storage
-            
-            localStorage.setItem("highScores", JSON.stringify(highScores));
-            window.location.assign("highscores.html")
-        }
-    });
-}
-let questions = [
-    {
-        question: "Who was the first president of the United States?",
-        choice1: "Abraham Lincoln",
-        choice2: "George Washington",
-        choice3: "Truman",
-        choice4: "Thomas Jefferson",
-        answer: 2
-    },
-    {
-        question:
-        "Which one is the largest animal in the world?",
-        choice1: "Blue Whale",
-        choice2: "Rhinoceros",
-        choice3: "Hippopotamus",
-        choice4: "Elephant",
-        answer: 1
-    },
-    {
-        question: "The planet near to the earth is?",
-        choice1: "Mercury",
-        choice2: "Jupiter",
-        choice3: "Mars",
-        choice4: "Saturn",
-        answer: 3
-    },
-    {
-        question: "The largest continent (Area wise) is?",
-        choice1: "Asia",
-        choice2: "Australia",
-        choice3: "North America",
-        choice4: "Africa",
-        answer: 1
-    },
-    {
-        question: "The USA consists of how many states?",
-        choice1: "52",
-        choice2: "50",
-        choice3: "48",
-        choice4: "53",
-        answer: 2
-    },
-];
-//END OF CODE FOR HARDCODED QUESTIONS-REMOVE EVERYTHING ABOVE WHEN API IS WORKING
-
-
-
 
 /*USE THIS CODE/FIX ONCE API IS UP
 
@@ -481,4 +284,200 @@ var mostRecentScore = localStorage.getItem('mostRecentScore');
     });
 }
 */
+
+//THIS CODE IS FOR THE HARD CODED QUESTIONS ONLY WHEN API NOT WORKING
+var selectedAmount;
+var selectedCategory;
+var selectedDifficulty;
+var mainPage = 'start';
+var timeLeft = 120;
+var totalScore = 0;
+const timerEl = document.getElementById("countdown");
+const timerElzero = document.getElementById("countdown-0");
+const scoreText = document.getElementById("score");
+const question = document.getElementById("question");
+const triviaEl = document.getElementById("game-container");
+const gameEl = document.getElementById("game");
+const choices = Array.from($(".choice-text"));
+const scorePage = document.getElementById("score-container");
+const displayScoreVal = document.getElementById("total-score");
+const submitScore = document.getElementById("submit-btn");
+const initials = document.getElementById("initials");
+const initialsEntered = document.getElementById('initials-entered');
+var highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the score, or the initial value if empty 
+const MAX_HIGH_SCORES = 5;
+let currentQuestion = {};
+let questionCounter = 0;
+let availableQuestions = [];
+let acceptingAnswers = false;//
+const MAX_QUESTIONS = 5;//
+
+//$("#game-container").hide();
+
+// start game when play button is clicked
+$("#playBtn").on("click", function(e) {
+    e.preventDefault();
+    document.getElementById(mainPage).style.display = "none";
+    triviaEl.classList.remove('hide');
+    //$("#game-container").show();
+    playGame();
+});
+//convert html Characters in array back to text before displaying
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+// start game with score reset to 0
+// if timer runs out 
+playGame = () => {
+    questionCounter = 0;
+    totalScore = 0;
+    availableQuestions = [...questions];
+    
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+
+    if (timeLeft === 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+getQuestion();
+};
+getQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //save score to local storage
+        localStorage.setItem("mostRecentScore", totalScore);
+        //go to the end to have user enter initials
+        displayScoreVal.textContent = totalScore;
+        endGame();
+    }
+    //display question and answer choices
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    var questionInput = currentQuestion.question;
+    var questionOutput = decodeHtml(questionInput);
+    question.innerText = questionOutput;
+    
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        var answerInput = currentQuestion["choice" + number]
+        var answerOutput = decodeHtml(answerInput);
+        choice.innerText = answerOutput;
+        });
+    
+        availableQuestions.splice(questionIndex, 1);
+        acceptingAnswers = true;
+};
+//add 10 points if correct answer selected, remove 5 points and 15 seconds if wrong answer selected, show selected answer as red/green based on corrent/wrong  
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+        acceptingAnswers = false;
+
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        const answerStatus =
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+        if (answerStatus === "correct") {
+            totalScore += 10;
+            //console.log("correct answer selcted, total score "+ totalScore);
+        } else {
+            //console.log("wrong answer selcted, total score "+ totalScore);
+            if(timeLeft >= 15) {
+                timeLeft -= 15;
+            } else {
+                timeLeft = 0;
+            }
+            if(totalScore >= 5) {
+                totalScore -= 5;
+                //console.log("total score "+ totalScore);
+            } else {
+                totalScore = 0;
+            }
+        }
+        scoreText.innerText = totalScore;
+        selectedChoice.parentElement.classList.add(answerStatus);       
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(answerStatus);
+            getQuestion();
+        }, 1000);
+    });
+});
+
+function endGame () {
+    gameEl.classList.add('hide');
+    scorePage.classList.remove('hide');
+
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+
+    submitScore.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const score = {
+            score: mostRecentScore,
+            initials: initials.value
+        };
+        if (initials.value === "") {
+            //window.alert("Initials cannot be blank");
+        } else {
+            highScores.push(score);
+
+            highScores.sort((a, b) => b.score - a.score); //sorts highscores in order
+            highScores.splice(5); //keeps top 5 scores in highscore local storage
+            
+            localStorage.setItem("highScores", JSON.stringify(highScores));
+            window.location.assign("highscores.html")
+        }
+    });
+}
+let questions = [
+    {
+        question: "Who was the first president of the United States?",
+        choice1: "Abraham Lincoln",
+        choice2: "George Washington",
+        choice3: "Truman",
+        choice4: "Thomas Jefferson",
+        answer: 2
+    },
+    {
+        question:
+        "Which one is the largest animal in the world?",
+        choice1: "Blue Whale",
+        choice2: "Rhinoceros",
+        choice3: "Hippopotamus",
+        choice4: "Elephant",
+        answer: 1
+    },
+    {
+        question: "The planet near to the earth is?",
+        choice1: "Mercury",
+        choice2: "Jupiter",
+        choice3: "Mars",
+        choice4: "Saturn",
+        answer: 3
+    },
+    {
+        question: "The largest continent (Area wise) is?",
+        choice1: "Asia",
+        choice2: "Australia",
+        choice3: "North America",
+        choice4: "Africa",
+        answer: 1
+    },
+    {
+        question: "The USA consists of how many states?",
+        choice1: "52",
+        choice2: "50",
+        choice3: "48",
+        choice4: "53",
+        answer: 2
+    },
+];
+//END OF CODE FOR HARDCODED QUESTIONS WHEN API IS WORKING
+
 
